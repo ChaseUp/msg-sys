cd0c30f606c83369836ca3184ba46fa72f8d039e
dereksunok <derek_sun_ok@163.com>
2016-10-28 07:33:28 +0800

    构建页面基础模块

--- messages/script/react.container.js
+++ messages/script/react.container.js
@@ -1,49 +1,41 @@
 var MessagesAside = React.createClass({
-	getInitialState:function(){		
-		return {data: []};
-	},
-	componentDidMount: function() {
-		$.ajax({
-			url: this.props.url,
-			type: 'get',
-			dataType: 'json',
-			success:function(xhr){
-				this.setState({data: xhr});
-			}.bind(this)
-		});
-	},
 	render:function(){
 		return (
-			<div className="hederInner">
-				<div>页头</div>
-				<div>通知 {this.state.data.notice}</div>
-				<div>用户名 {this.state.data.userName}</div>
+			<div id="aside">
+				左侧栏
 			</div>
 			);
 	}
 });
 
-var MessagesMain = React.createClass({
-	getInitialState:function(){		
-		return {data: []};
-	},
-	componentDidMount: function() {
-		$.ajax({
-			url: this.props.url,
-			type: 'get',
-			dataType: 'json',
-			success:function(xhr){
-				this.setState({data: xhr});
-			}.bind(this)
-		});
-	},
+var MessagesContainer = React.createClass({
 	render:function(){
 		return (
-			<div className="hederInner">
-				<div>页头</div>
-				<div>通知 {this.state.data.notice}</div>
-				<div>用户名 {this.state.data.userName}</div>
-			</div>
-			);
+			<div className="containerInner">
+				<MessagesAside />
+				<div id="main">
+					<MessagesInputArea />
+					<MessagesListTabs />
+				</div>
+			</div>);
 	}
 });
+
+var MessagesInputArea = React.createClass({
+	render:function(){
+		return (
+			<div className="msgInputArea"> 消息输入框！</div>
+			);
+	}
+})
+
+var MessagesListTabs = React.createClass({
+	render:function(){
+		return (
+			<div className="msgListTabs">消息列表！</div>
+		);
+	}
+});
+
+ReactDOM.render(<MessagesContainer />,document.getElementById('container'));
+
