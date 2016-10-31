c4923d364d4143b97b8cf1d15a4a97facb56e1a4
dereksunok <derek_sun_ok@163.com>
2016-10-31 15:11:55 +0800

    添加保存 && 草稿按钮

--- messages/script/react.main.js
+++ messages/script/react.main.js
@@ -1,17 +1,52 @@
-var MessagesMain = React.createClass({
-	render:function(){
-		return (
-			<div className="mainInner fix">
-				<MessagesInputArea />
-				<MessagesListTabs />
-			</div>);
-	}
-});
-
 var MessagesInputArea = React.createClass({
 	render:function(){
 		return (
-			<div className="msgInputArea">消息输入框</div>
+				<div className="msgInputArea sections">
+					<div className="socialBtns fix">
+						<div className="socialBtnsCheck fll">
+							<div className="checkBtn facebook">
+								<i className="fa fa-facebook"></i>
+								<span><i className="fa fa-check"></i></span>
+							</div>
+							<div className="checkBtn twitter">
+								<i className="fa fa-twitter"></i>
+								<span className="hide"><i className="fa fa-check"></i></span>
+							</div>
+							<div className="checkBtn LinkedIn">
+								<i className="fa fa-linkedin"></i>
+								<span className="hide"><i className="fa fa-check"></i></span>
+							</div>
+						</div>
+						<div className="textSizeCheck flr">
+							<span className="textSizeTip">还可以输入：</span>
+							<div className="checkBtn facebook small"><i className="fa fa-facebook"></i></div>
+							<ins className="error">140</ins>
+							<div className="checkBtn twitter small"><i className="fa fa-twitter"></i></div>
+							<ins>200</ins>
+							<div className="checkBtn LinkedIn small"><i className="fa fa-linkedin"></i></div>
+							<ins>600</ins>
+						</div>
+					</div>
+
+					<div className="messageInputSec mt10">
+						<div className="messageInputWrap">
+							<textarea name="messageInput" id="messageInput"></textarea>
+						</div>
+						<div className="messageInputTools mt10 fix">
+							<div className="toolBtns fll">
+								<a href="javascript:;" className="photos"><i className="fa fa-photo" aria-hidden="true"></i> 图片</a>
+								<a href="javascript:;" className="articles"><i className="fa fa-newspaper-o" aria-hidden="true"></i> 引用文章</a>
+								<a href="javascript:;" className="products"><i className="fa fa-cubes" aria-hidden="true"></i> 引用产品</a>
+								<a href="javascript:;" className="pages"><i className="fa fa-file-text-o" aria-hidden="true"></i> 引用页面</a>
+								<a href="javascript:;" className="clockPush"><i className="fa fa-clock-o" aria-hidden="true"></i> 定时发</a>
+							</div>
+							<div className="postBtns flr">
+								<a href="javascript:;" className="postBtns-draft">保存草稿</a>
+								<a href="javascript:;" className="postBtns-push">发布</a>
+							</div>
+						</div>
+					</div>
+				</div>
 			);
 	}
 })
@@ -21,6 +56,16 @@ var MessagesListTabs = React.createClass({
 		return (
 			<div className="msgListTabs">消息列表</div>
 		);
+	}
+});
+
+var MessagesMain = React.createClass({
+	render:function(){
+		return (
+			<div className="mainInner">
+				<MessagesInputArea />
+				<MessagesListTabs />
+			</div>);
 	}
 });
 
